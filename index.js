4d46e07e16c72e783d42a1ce5d9fd355e87eb595
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-20 16:57:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let store = { users: [], items: [] };
 // initialize store with key of items and users that each point to an empty array
- 
-let userId = 0;
+
+let driverId = 0;
 
 class Driver{
   constructor(name) {
