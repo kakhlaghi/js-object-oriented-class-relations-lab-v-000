1503e1ea05dad7aed21c00889315f07d6ea74e61
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-20 19:52:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ class Passenger{
   setTrip(trip){
     this.tripId = trip.id
     }
-  trip(){
+  trips(){
     return store.trips.find(
       function(trip){
         return trip.id == this.tripId;
