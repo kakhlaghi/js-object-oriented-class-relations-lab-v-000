2f29772a4706a251a125d6bfbcbb77bcad5e728f
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-20 19:05:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,4 +44,15 @@ class Trip{
     )
   }
 
+  setDriver(driver){
+    this.driverId = driver.id
+  }
+  driver(){
+    return store.drivers.find(
+      function(driver){
+        return driver.id == this.driverId;
+      }.bind(this)
+    )
+  }
+
 }
